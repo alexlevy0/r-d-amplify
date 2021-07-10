716e7be6d60c0d37c4364a4831ad7decfc58dd1c
Alex Lévy <alexlevy0@gmail.com>
2021-07-10 08:41:45 +0200

    add pinpoint and edit schema

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { View, Text, StyleSheet, TextInput, Button } from 'react-native'
-import Amplify, { API, graphqlOperation } from 'aws-amplify'
+import Amplify, { Analytics, API, graphqlOperation } from 'aws-amplify'
 import { withAuthenticator } from 'aws-amplify-react-native'
 
 import config from './src/aws-exports'
@@ -8,6 +8,35 @@ import { createTodo } from './src/graphql/mutations'
 import { listTodos } from './src/graphql/queries'
 
 Amplify.configure(config)
+Analytics.record({ name: 'Opened' });
+Analytics.autoTrack('session', {
+  // REQUIRED, turn on/off the auto tracking
+  enable: true,
+  // OPTIONAL, the attributes of the event, you can either pass an object or a function 
+  // which allows you to define dynamic attributes
+  attributes: {
+    attr: 'attr'
+  },
+  // when using function
+  // attributes: () => {
+  //    const attr = somewhere();
+  //    return {
+  //        myAttr: attr
+  //    }
+  // },
+  // OPTIONAL, the service provider, by default is the Amazon Pinpoint
+  provider: 'AWSPinpoint',
+
+  // 2 following lines is not supported in React Native.
+
+  // OPTIONAL, by default is 'multiPageApp'
+  // you need to change it to 'SPA' if your app is a single-page app like React
+  type: 'multiPageApp',
+  getUrl: () => {
+    // the default function
+    return window.location.origin + window.location.pathname;
+  }
+});
 
 const App = () => {
   const initialState = { name: '', description: '' }
